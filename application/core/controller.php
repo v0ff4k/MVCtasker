dd9bd064893b34861fcf3c034d7c42bd6a9d3dee
Vladimir Che <v0ff4k@users.noreply.github.com>
2017-03-13 18:57:47 +0600

    added controller.php

---
+++ application/core/controller.php
@@ -0,0 +1,48 @@
+<?php
+
+class Controller
+{
+    /**
+     * @var null Database Connection
+     */
+    public $db = null;
+
+    /**
+     * @var null Model
+     */
+    public $model = null;
+
+    /**
+     * Whenever controller is created, open a database connection too and load "the model".
+     */
+    function __construct()
+    {
+        $this->openDatabaseConnection();
+        $this->loadModel();
+    }
+
+    /**
+     * Open the database connection with the credentials from config/config.php
+     */
+    private function openDatabaseConnection()
+    {
+        $options = array(
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, 
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
+        
+        $this->db = new PDO(DB_TYPE . ':host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=' . DB_CHARSET, 
+            DB_USER, 
+            DB_PASS, 
+            $options);
+    }
+
+    /**
+     * Loads the "model".
+     * @return object model
+     */
+    public function loadModel()
+    {
+        require APP . 'model/Model.php';
+        $this->model = new Model($this->db);
+    }
+}
